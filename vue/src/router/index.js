280dd8bf3ddd9491ba4db54d729f425d93840920
Gameiro <alexandrefgame@gmail.com>
2024-11-17 14:54:27 +0000

    Added the provided files to deploy on server

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,11 +1,30 @@
-import { createRouter, createWebHistory } from "vue-rounter"
+import HomeComponent from '@/components/HomeComponent.vue'
+import LaravelTester from '@/components/LaravelTester.vue'
+import WebSocketTester from '@/components/WebSocketTester.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeComponent
+    },
+    {
+      path: '/testers',
+      children: [
         {
-            path: "/",
-            name: "home"
+          path: 'laravel',
+          component: LaravelTester
+        },
+        {
+          path: 'websocket',
+          component: WebSocketTester
         }
-    ]
+      ]
+    }
+  ]
 })
+
+export default router
