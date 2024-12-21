52c27e0395d3053cddcd5e1ea2543e09a5f825ff
João Palma <palmajoaodiogo@gmail.com>
2024-12-21 19:13:13 +0000

    fixed time in history

--- laravel/app/Http/Controllers/GameController.php
+++ laravel/app/Http/Controllers/GameController.php
@@ -22,18 +22,18 @@ class GameController extends Controller
         // Filter games that have ended and are associated with a valid user
         $query = Game::whereHas('creator');
 
-    // Apply additional filters using readAttributes if present
-    $attributesResponse = $this->readAttributes($request, $query);
+        // Apply additional filters using readAttributes if present
+        $attributesResponse = $this->readAttributes($request, $query);
 
-    // Return the response from readAttributes or a paginated collection of games
-    return $attributesResponse ?? GameResource::collection($query->paginate(10));
-  }
+        // Return the response from readAttributes or a paginated collection of games
+        return $attributesResponse ?? GameResource::collection($query->paginate(10));
+    }
 
     public function indexEndedGames(GameTypeRequest $request)
     {
         // Filter games that have ended and are associated with a valid user
         $query = Game::where('status', GAME::STATUS_ENDED)
-        ->whereHas('creator');
+            ->whereHas('creator');
 
         // Apply additional filters using readAttributes if present
         $attributesResponse = $this->readAttributes($request, $query);
@@ -58,13 +58,12 @@ class GameController extends Controller
             })
             ->when($type == Game::TYPE_MULTIPLAYER, function ($query) use ($userId) {
                 return $query->where('type', Game::TYPE_MULTIPLAYER);
-            })
-            ->where('status', Game::STATUS_ENDED);  // Always filter by ended games
+            });
 
-    $attributesResponse = $this->readAttributes($request, $query);
+        $attributesResponse = $this->readAttributes($request, $query);
 
-    return $attributesResponse ?? GameResource::collection($query->paginate(10));
-  }
+        return $attributesResponse ?? GameResource::collection($query->paginate(10));
+    }
 
     public function store(CreateGameRequest $request)
     {
@@ -105,8 +104,8 @@ class GameController extends Controller
 
         $game->began_at = $time->isoFormat("YYYY-mm-DD HH:MM:ss");
 
-    return new GameResource($game);
-  }
+        return new GameResource($game);
+    }
 
     public function update(UpdateGameRequest $request, Game $game)
     {
@@ -116,8 +115,8 @@ class GameController extends Controller
         if($game->status == Game::STATUS_ENDED || $game->status == Game::STATUS_INTERRUPTED)
         return response()->json(['message' => 'Cannot update ended or interrupted games'], 400);
 
-    if ($game->status == Game::STATUS_PROGRESS && $request->status == Game::STATUS_PENDING)
-      return response()->json(['message' => 'Cannot put a game in progress back to pending'], 400);
+        if ($game->status == Game::STATUS_PROGRESS && $request->status == Game::STATUS_PENDING)
+        return response()->json(['message' => 'Cannot put a game in progress back to pending'], 400);
 
         $requestValidated = $request->validated();
 
@@ -127,88 +126,87 @@ class GameController extends Controller
 
         $game->save();
 
-    return new GameResource($game);
-  }
-
-  private function readAttributes(Request $request, $query)
-  {
-    // Filter by board
-    if ($request->has('board')) {
-      $board_size = explode("x", $request->query('board'));
-      $board = Board::where('board_cols', $board_size[0])->where('board_rows', $board_size[1])->first();
-      if ($board == null)
-        return response()->json(['message' => 'Invalid board size: board not found'], 404);
-      $query->where('board_id', $board->id);
+        return new GameResource($game);
     }
 
-    // Order by either number of turns or time taken (score)
-    $isOrderedByScore = false;
-    if ($request->has('score_type')) {
-      $isOrderedByScore = true;
-      if ($request->query('score_type') === 'time')
+    private function readAttributes(Request $request, $query)
+    {
+        // Filter by board
+        if ($request->has('board')) {
+            $board_size = explode("x", $request->query('board'));
+            $board = Board::where('board_cols', $board_size[0])->where('board_rows', $board_size[1])->first();
+            if ($board == null)
+            return response()->json(['message' => 'Invalid board size: board not found'], 404);
+            $query->where('board_id', $board->id);
+        }
+
+        // Order by either number of turns or time taken (score)
+        if ($request->has('score_type')) {
+            if ($request->query('score_type') === 'time')
         $query->orderBy('total_time', 'asc')->orderBy('ended_at', 'asc')->orderBy('total_turns_winner', 'asc');
-      else if ($request->query('score_type') === 'turns')
+            else if ($request->query('score_type') === 'turns')
         $query->orderBy('total_turns_winner', 'asc')->orderBy('ended_at', 'asc')->orderBy('total_time', 'asc');
-    }
+        }
+        else{
+            $query->orderBy('began_at', 'desc');
+        }
 
-    // Filter by type, defaults to singleplayer
-    $type = $request->query('type');
-    if ($type != 'A')
-      $query->where('type', $type ?? Game::TYPE_SINGLEPLAYER);
-
-    // Filter by a start date (all games played after it are gotten)
-    if ($request->has('date_start')) {
-      $dateStart = $request->query('date_start');
-      $parsedDateStart = \DateTime::createFromFormat('d-m-Y', $dateStart);
+        // Filter by type, defaults to singleplayer
+        $type = $request->query('type');
+        if ($type != 'A')
+        $query->where('type', $type ?? Game::TYPE_SINGLEPLAYER);
 
         // Filter by a start date (all games played after it are gotten)
-        if($request->has('date_start')){
+        if ($request->has('date_start')) {
             $dateStart = $request->query('date_start');
             $parsedDateStart = \DateTime::createFromFormat('d-m-Y', $dateStart);
 
-            if($parsedDateStart)
-            $query->where('ended_at', '>=', $parsedDateStart->format('Y-m-d'));
+            // Filter by a start date (all games played after it are gotten)
+            if($request->has('date_start')){
+                $dateStart = $request->query('date_start');
+                $parsedDateStart = \DateTime::createFromFormat('d-m-Y', $dateStart);
 
-            else
-            return response()->json(['message' => 'Invalid \'date_start\' date format: must be DD-mm-YYYY'], 402);
+                if($parsedDateStart)
+                $query->where('ended_at', '>=', $parsedDateStart->format('Y-m-d'));
+
+                else
+                return response()->json(['message' => 'Invalid \'date_start\' date format: must be DD-mm-YYYY'], 402);
+            }
+
+            // Filter by end date (all games played before it are gotten)
+            if($request->has('date_end')){
+                $dateEnd = $request->query('date_end');
+                $parsedDateEnd = \DateTime::createFromFormat('d-m-Y', $dateEnd);
+
+                if($parsedDateEnd)
+                $query->where('ended_at', '<=', $parsedDateEnd->format('Y-m-d').' 23:59:59');
+
+                else
+                return response()->json(['message' => 'Invalid \'date_end\' date format: must be DD-mm-YYYY'], 402);
+            }
         }
 
         // Filter by end date (all games played before it are gotten)
-        if($request->has('date_end')){
+        if ($request->has('date_end')) {
             $dateEnd = $request->query('date_end');
             $parsedDateEnd = \DateTime::createFromFormat('d-m-Y', $dateEnd);
 
-            if($parsedDateEnd)
-            $query->where('ended_at', '<=', $parsedDateEnd->format('Y-m-d').' 23:59:59');
-
+            if ($parsedDateEnd)
+            $query->where('ended_at', '<=', $parsedDateEnd->format('Y-m-d') . ' 23:59:59');
             else
             return response()->json(['message' => 'Invalid \'date_end\' date format: must be DD-mm-YYYY'], 402);
         }
-        // Ordering by most recent or by oldest if score ordering was parsed
-        $query->orderBy('began_at', $isOrderedByScore ? 'asc' : 'desc');
     }
+    private function gameTransaction($userId, $gameId, $type, $amount, $time)
+    {
+        $transaction = new Transaction();
+        $transaction->user_id = $userId;
+        $transaction->game_id = $gameId;
+        $transaction->type = $type;
+        $transaction->brain_coins = $amount;
+        $transaction->transaction_datetime = $time;
 
-    // Filter by end date (all games played before it are gotten)
-    if ($request->has('date_end')) {
-      $dateEnd = $request->query('date_end');
-      $parsedDateEnd = \DateTime::createFromFormat('d-m-Y', $dateEnd);
-
-      if ($parsedDateEnd)
-        $query->where('ended_at', '<=', $parsedDateEnd->format('Y-m-d') . ' 23:59:59');
-      else
-        return response()->json(['message' => 'Invalid \'date_end\' date format: must be DD-mm-YYYY'], 402);
+        $transaction->save();
+        return $transaction;
     }
-  }
-  private function gameTransaction($userId, $gameId, $type, $amount, $time)
-  {
-      $transaction = new Transaction();
-      $transaction->user_id = $userId;
-      $transaction->game_id = $gameId;
-      $transaction->type = $type;
-      $transaction->brain_coins = $amount;
-      $transaction->transaction_datetime = $time;
-
-      $transaction->save();
-      return $transaction;
-  }
 }
